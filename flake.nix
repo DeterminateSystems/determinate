5b6683bff716d61fb598d446e2a8437019ea1e99
Graham Christensen <graham@grahamc.com>
2024-07-29 10:12:04 -0400

    NixOS module: drop the nix module, we can't release it properly.

--- flake.nix
+++ flake.nix
@@ -169,10 +169,6 @@
 
 
       nixosModules.default = { lib, pkgs, config, ... }: {
-        imports = [
-          inputs.nix.nixosModules.default
-        ];
-
         options = {
           determinate.nix.primaryUser.username = lib.mkOption {
             type = lib.types.str;
@@ -199,13 +195,32 @@
             inputs.fh.packages."${pkgs.stdenv.system}".default
           ];
 
+          nix.package = inputs.nix.packages."${pkgs.stdenv.system}".default;
+
+          nix.registry.nixpkgs = {
+            exact = true;
+            from = {
+              type = "indirect";
+              id = "nixpkgs";
+            };
+            to = {
+              type = "tarball";
+              url = "https://flakehub.com/f/DeterminateSystems/nixpkgs-weekly/0.1.0.tar.gz";
+            };
+          };
+
           nix.settings = {
-            netrc-file = config.determinate.nix.primaryUser.netrcPath;
+            always-allow-substitutes = true;
+            bash-prompt-prefix = "(nix:$name)\\040";
+            experimental-features = [ "nix-command" "flakes" ];
+            extra-nix-path = [ "nixpkgs=flake:nixpkgs" ];
             extra-substituters = [ "https://cache.flakehub.com" ];
             extra-trusted-public-keys = [
               "cache.flakehub.com-1:t6986ugxCA+d/ZF9IeMzJkyqi5mDhvFIx7KA/ipulzE="
               "cache.flakehub.com-2:ntBGiaKSmygJOw2j1hFS7KDlUHQWmZALvSJ9PxMJJYU="
             ];
+            netrc-file = config.determinate.nix.primaryUser.netrcPath;
+            upgrade-nix-store-path-url = "https://install.determinate.systems/nix-upgrade/stable/universal";
           };
         };
       };
