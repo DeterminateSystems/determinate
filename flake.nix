005457cdb7d26ae270408ae6207b49723f02144e
Cole Helbling <cole.helbling@determinate.systems>
2024-08-15 12:50:56 -0700

    Deduplicate settings shared between NixOS, home-manager, and nix-darwin into reusable modules

--- flake.nix
+++ flake.nix
@@ -44,6 +44,64 @@
       # Stronger than mkDefault (1000), weaker than mkForce (50) and the "default override priority"
       # (100).
       mkPreferable = inputs.nixpkgs.lib.mkOrder 750;
+
+      # Common settings that are shared between NixOS, home-manager, and nix-darwin modules.
+      # The settings configured in this module must be generally settable by users both trusted and
+      # untrusted by the Nix daemon. Settings that require being a trusted user belong in the
+      # `restrictedSettingsModule` below.
+      commonSettingsModule = { config, pkgs, lib, ... }: {
+        nix.package = inputs.nix.packages."${pkgs.stdenv.system}".default;
+
+        nix.registry.nixpkgs = {
+          exact = true;
+
+          from = {
+            type = "indirect";
+            id = "nixpkgs";
+          };
+
+          # NOTE(cole-h): The NixOS module exposes a `flake` option that is a fancy wrapper around
+          # setting `to` -- we don't want to clobber this if users have set it on their own
+          to = lib.mkIf (config.nix.registry.nixpkgs.flake or null == null) (mkPreferable {
+            type = "tarball";
+            url = "https://flakehub.com/f/DeterminateSystems/nixpkgs-weekly/0.1.0.tar.gz";
+          });
+        };
+
+        nix.settings = {
+          bash-prompt-prefix = "(nix:$name)\\040";
+          extra-experimental-features = [ "nix-command" "flakes" ];
+          extra-nix-path = [ "nixpkgs=flake:nixpkgs" ];
+          extra-substituters = [ "https://cache.flakehub.com" ];
+        };
+      };
+
+      # Restricted settings that are shared between NixOS and nix-darwin modules. This is
+      # specifically not used in the home-manager module because otherwise it would cause infinite
+      # recursion (because of some interaction with `lib.optionals` and this module).
+      # The settings configured in this module require being a user trusted by the Nix daemon.
+      restrictedSettingsModule = { ... }: {
+        nix.settings = restrictedNixSettings;
+      };
+
+      # Nix settings that require being a trusted user to configure.
+      # NOTE(cole-h): we can't merge this with the `restrictedSettingsModule` above without causing
+      # infinite recursion in the home-manager module.
+      restrictedNixSettings = {
+        always-allow-substitutes = true;
+        netrc-file = "/nix/var/determinate/netrc";
+        upgrade-nix-store-path-url = "https://install.determinate.systems/nix-upgrade/stable/universal";
+        extra-trusted-public-keys = [
+          "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
+          "cache.flakehub.com-4:Asi8qIv291s0aYLyH6IOnr5Kf6+OF14WVjkE6t3xMio="
+          "cache.flakehub.com-5:zB96CRlL7tiPtzA9/WKyPkp3A2vqxqgdgyTVNGShPDU="
+          "cache.flakehub.com-6:W4EGFwAGgBj3he7c5fNh9NkOXw0PUVaxygCVKeuvaqU="
+          "cache.flakehub.com-7:mvxJ2DZVHn/kRxlIaxYNMuDG1OvMckZu32um1TadOR8="
+          "cache.flakehub.com-8:moO+OVS0mnTjBTcOUh2kYLQEd59ExzyoW1QgQ8XAARQ="
+          "cache.flakehub.com-9:wChaSeTI6TeCuV/Sg2513ZIM9i0qJaYsF+lZCXg0J6o="
+          "cache.flakehub.com-10:2GqeNlIp6AKp4EF2MVbE1kBOp9iBSyo0UPR9KoR0o1Y="
+        ];
+      };
     in
     {
       packages = forAllSystems ({ system, pkgs, ... }: {
@@ -81,84 +139,32 @@
           };
         };
 
+        imports = [
+          commonSettingsModule
+        ];
+
         config = {
           home.packages = [
             inputs.fh.packages."${pkgs.stdenv.system}".default
             config.nix.package
           ];
 
-          nix.package = inputs.nix.packages."${pkgs.stdenv.system}".default;
-
-          nix.registry.nixpkgs = {
-            exact = true;
-
-            from = {
-              type = "indirect";
-              id = "nixpkgs";
-            };
-
-            # NOTE(cole-h): The NixOS module exposes a `flake` option that is a fancy wrapper around
-            # setting `to` -- we don't want to clobber this if users have set it on their own
-            to = lib.mkIf (config.nix.registry.nixpkgs.flake or null == null) (mkPreferable {
-              type = "tarball";
-              url = "https://flakehub.com/f/DeterminateSystems/nixpkgs-weekly/0.1.0.tar.gz";
-            });
-          };
-
-          # The `extra-trusted-public-keys` settings are privileged and so
-          # they're applied only if `primaryUser.isTrusted` is set to `true`
-          nix.settings = lib.mkMerge [
-            (lib.optionalAttrs (config.determinate.nix.primaryUser.isTrusted) {
-              always-allow-substitutes = true;
-              extra-trusted-public-keys = [
-                "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
-                "cache.flakehub.com-4:Asi8qIv291s0aYLyH6IOnr5Kf6+OF14WVjkE6t3xMio="
-                "cache.flakehub.com-5:zB96CRlL7tiPtzA9/WKyPkp3A2vqxqgdgyTVNGShPDU="
-                "cache.flakehub.com-6:W4EGFwAGgBj3he7c5fNh9NkOXw0PUVaxygCVKeuvaqU="
-                "cache.flakehub.com-7:mvxJ2DZVHn/kRxlIaxYNMuDG1OvMckZu32um1TadOR8="
-                "cache.flakehub.com-8:moO+OVS0mnTjBTcOUh2kYLQEd59ExzyoW1QgQ8XAARQ="
-                "cache.flakehub.com-9:wChaSeTI6TeCuV/Sg2513ZIM9i0qJaYsF+lZCXg0J6o="
-                "cache.flakehub.com-10:2GqeNlIp6AKp4EF2MVbE1kBOp9iBSyo0UPR9KoR0o1Y="
-              ];
-              netrc-file = "/nix/var/determinate/netrc";
-              upgrade-nix-store-path-url = "https://install.determinate.systems/nix-upgrade/stable/universal";
-            })
-            {
-              bash-prompt-prefix = "(nix:$name)\\040";
-              experimental-features = [ "nix-command" "flakes" ];
-              extra-nix-path = [ "nixpkgs=flake:nixpkgs" ];
-              extra-substituters = [ "https://cache.flakehub.com" ];
-            }
-          ];
+          nix.settings = lib.optionalAttrs config.determinate.nix.primaryUser.isTrusted restrictedNixSettings;
         };
       };
 
       darwinModules.default = { lib, config, pkgs, ... }: {
+        imports = [
+          commonSettingsModule
+          restrictedSettingsModule
+        ];
+
         config = {
           environment.systemPackages = [
             inputs.fh.packages."${pkgs.stdenv.system}".default
           ];
 
           services.nix-daemon.enable = true;
-
-          nix.package = inputs.nix.packages."${pkgs.stdenv.system}".default;
-
-          nix.registry.nixpkgs = {
-            exact = true;
-
-            from = {
-              type = "indirect";
-              id = "nixpkgs";
-            };
-
-            # NOTE(cole-h): The NixOS module exposes a `flake` option that is a fancy wrapper around
-            # setting `to` -- we don't want to clobber this if users have set it on their own
-            to = lib.mkIf (config.nix.registry.nixpkgs.flake or null == null) (mkPreferable {
-              type = "tarball";
-              url = "https://flakehub.com/f/DeterminateSystems/nixpkgs-weekly/0.1.0.tar.gz";
-            });
-          };
-
           launchd.daemons.nix-daemon.serviceConfig = {
             StandardErrorPath = lib.mkForce "/var/log/determinate-nixd.log";
             StandardOutPath = lib.mkForce "/var/log/determinate-nixd.log";
@@ -187,52 +193,20 @@
             HardResourceLimits.NumberOfFiles = 2097152;
           };
 
-          nix.settings = {
-            always-allow-substitutes = true;
-            bash-prompt-prefix = "(nix:$name)\\040";
-            experimental-features = [ "nix-command" "flakes" ];
-            extra-nix-path = [ "nixpkgs=flake:nixpkgs" ];
-            extra-substituters = [ "https://cache.flakehub.com" ];
-            extra-trusted-public-keys = [
-              "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
-              "cache.flakehub.com-4:Asi8qIv291s0aYLyH6IOnr5Kf6+OF14WVjkE6t3xMio="
-              "cache.flakehub.com-5:zB96CRlL7tiPtzA9/WKyPkp3A2vqxqgdgyTVNGShPDU="
-              "cache.flakehub.com-6:W4EGFwAGgBj3he7c5fNh9NkOXw0PUVaxygCVKeuvaqU="
-              "cache.flakehub.com-7:mvxJ2DZVHn/kRxlIaxYNMuDG1OvMckZu32um1TadOR8="
-              "cache.flakehub.com-8:moO+OVS0mnTjBTcOUh2kYLQEd59ExzyoW1QgQ8XAARQ="
-              "cache.flakehub.com-9:wChaSeTI6TeCuV/Sg2513ZIM9i0qJaYsF+lZCXg0J6o="
-              "cache.flakehub.com-10:2GqeNlIp6AKp4EF2MVbE1kBOp9iBSyo0UPR9KoR0o1Y="
-            ];
-            netrc-file = "/nix/var/determinate/netrc";
-            upgrade-nix-store-path-url = "https://install.determinate.systems/nix-upgrade/stable/universal";
-          };
         };
       };
 
 
       nixosModules.default = { lib, pkgs, config, ... }: {
+        imports = [
+          commonSettingsModule
+          restrictedSettingsModule
+        ];
+
         config = {
           environment.systemPackages = [
             inputs.fh.packages."${pkgs.stdenv.system}".default
           ];
-
-          nix.package = inputs.nix.packages."${pkgs.stdenv.system}".default;
-
-          nix.registry.nixpkgs = {
-            exact = true;
-
-            from = {
-              type = "indirect";
-              id = "nixpkgs";
-            };
-
-            # NOTE(cole-h): The NixOS module exposes a `flake` option that is a fancy wrapper around
-            # setting `to` -- we don't want to clobber this if users have set it on their own
-            to = lib.mkIf (config.nix.registry.nixpkgs.flake or null == null) (mkPreferable {
-              type = "tarball";
-              url = "https://flakehub.com/f/DeterminateSystems/nixpkgs-weekly/0.1.0.tar.gz";
-            });
-          };
 
           systemd.services.nix-daemon.serviceConfig.ExecStart = [
             ""
@@ -255,26 +229,6 @@
               FileDescriptorName = "determinate-nixd.socket";
               ListenStream = "/nix/var/determinate/determinate-nixd.socket";
             };
-          };
-
-          nix.settings = {
-            always-allow-substitutes = true;
-            bash-prompt-prefix = "(nix:$name)\\040";
-            experimental-features = [ "nix-command" "flakes" ];
-            extra-nix-path = [ "nixpkgs=flake:nixpkgs" ];
-            extra-substituters = [ "https://cache.flakehub.com" ];
-            extra-trusted-public-keys = [
-              "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
-              "cache.flakehub.com-4:Asi8qIv291s0aYLyH6IOnr5Kf6+OF14WVjkE6t3xMio="
-              "cache.flakehub.com-5:zB96CRlL7tiPtzA9/WKyPkp3A2vqxqgdgyTVNGShPDU="
-              "cache.flakehub.com-6:W4EGFwAGgBj3he7c5fNh9NkOXw0PUVaxygCVKeuvaqU="
-              "cache.flakehub.com-7:mvxJ2DZVHn/kRxlIaxYNMuDG1OvMckZu32um1TadOR8="
-              "cache.flakehub.com-8:moO+OVS0mnTjBTcOUh2kYLQEd59ExzyoW1QgQ8XAARQ="
-              "cache.flakehub.com-9:wChaSeTI6TeCuV/Sg2513ZIM9i0qJaYsF+lZCXg0J6o="
-              "cache.flakehub.com-10:2GqeNlIp6AKp4EF2MVbE1kBOp9iBSyo0UPR9KoR0o1Y="
-            ];
-            netrc-file = "/nix/var/determinate/netrc";
-            upgrade-nix-store-path-url = "https://install.determinate.systems/nix-upgrade/stable/universal";
           };
         };
       };
