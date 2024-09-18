587b3ffeffca2183f30876b288267cd3c25a4842
Graham Christensen <graham@grahamc.com>
2024-09-18 13:15:58 -0400

    Bump inputs

--- flake.nix
+++ flake.nix
@@ -6,15 +6,15 @@
     nixpkgs.url = "https://flakehub.com/f/DeterminateSystems/nixpkgs-weekly/0.1.tar.gz";
 
     determinate-nixd-aarch64-linux = {
-      url = "https://install.determinate.systems/determinate-nixd/rev/b9628803e10c0063fdc4973e9eaf1e9d90caa107/aarch64-linux";
+      url = "https://install.determinate.systems/determinate-nixd/rev/2c18a8f38492d35be64d4e497b720938f17cc9f5/aarch64-linux";
       flake = false;
     };
     determinate-nixd-x86_64-linux = {
-      url = "https://install.determinate.systems/determinate-nixd/rev/b9628803e10c0063fdc4973e9eaf1e9d90caa107/x86_64-linux";
+      url = "https://install.determinate.systems/determinate-nixd/rev/2c18a8f38492d35be64d4e497b720938f17cc9f5/x86_64-linux";
       flake = false;
     };
     determinate-nixd-aarch64-darwin = {
-      url = "https://install.determinate.systems/determinate-nixd/rev/b9628803e10c0063fdc4973e9eaf1e9d90caa107/macOS";
+      url = "https://install.determinate.systems/determinate-nixd/rev/2c18a8f38492d35be64d4e497b720938f17cc9f5/macOS";
       flake = false;
     };
     determinate-nixd-x86_64-darwin.follows = "determinate-nixd-aarch64-darwin";
