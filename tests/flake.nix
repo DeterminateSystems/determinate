fbdf1741332e844d92c326faef812e46a1a21e01
Luc Perkins <lucperkins@gmail.com>
2025-10-12 21:56:59 -0500

    Apply statix suggestions

--- tests/flake.nix
+++ tests/flake.nix
@@ -14,58 +14,62 @@
       ...
     }@inputs:
     {
-      checks.x86_64-linux.nixos =
-        (inputs.nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            inputs.determinate.nixosModules.default
-            {
-              fileSystems."/" = {
-                device = "/dev/bogus";
-                fsType = "ext4";
-              };
-              boot.loader.grub.devices = [ "/dev/bogus" ];
-              system.stateVersion = "24.11";
-            }
-          ];
-        }).config.system.build.toplevel;
-
-      checks.aarch64-darwin.nix-darwin =
-        (inputs.nix-darwin.lib.darwinSystem {
-          system = "aarch64-darwin";
-
-          modules = [
-            inputs.determinate.darwinModules.default
-            {
-              determinateNix.enable = true;
-              system.stateVersion = 5;
-            }
-          ];
-        }).system;
-
-      checks.aarch64-darwin.nix-darwin-custom-config =
-        (inputs.nix-darwin.lib.darwinSystem {
-          system = "aarch64-darwin";
-
-          modules = [
-            inputs.determinate.darwinModules.default
-            {
-              determinateNix = {
-                enable = true;
-                customSettings = {
-                  auto-optimise-store = true;
-                  extra-experimental-features = [ "build-time-fetch-tree" ];
-                  flake-registry = "/etc/nix/flake-registry.json";
+      checks = {
+        x86_64-linux.nixos =
+          (inputs.nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+              inputs.determinate.nixosModules.default
+              {
+                fileSystems."/" = {
+                  device = "/dev/bogus";
+                  fsType = "ext4";
                 };
-                determinateNixd = {
-                  builder.state = "disabled";
-                  authentication.additionalNetrcSources = [ "/etc/extra/netrc" ];
-                  garbageCollector.strategy = "disabled";
-                };
-              };
-              system.stateVersion = 5;
-            }
-          ];
-        }).system;
+                boot.loader.grub.devices = [ "/dev/bogus" ];
+                system.stateVersion = "24.11";
+              }
+            ];
+          }).config.system.build.toplevel;
+
+        aarch64-darwin = {
+          nix-darwin =
+            (inputs.nix-darwin.lib.darwinSystem {
+              system = "aarch64-darwin";
+
+              modules = [
+                inputs.determinate.darwinModules.default
+                {
+                  determinateNix.enable = true;
+                  system.stateVersion = 5;
+                }
+              ];
+            }).system;
+
+          nix-darwin-custom-config =
+            (inputs.nix-darwin.lib.darwinSystem {
+              system = "aarch64-darwin";
+
+              modules = [
+                inputs.determinate.darwinModules.default
+                {
+                  determinateNix = {
+                    enable = true;
+                    customSettings = {
+                      auto-optimise-store = true;
+                      extra-experimental-features = [ "build-time-fetch-tree" ];
+                      flake-registry = "/etc/nix/flake-registry.json";
+                    };
+                    determinateNixd = {
+                      builder.state = "disabled";
+                      authentication.additionalNetrcSources = [ "/etc/extra/netrc" ];
+                      garbageCollector.strategy = "disabled";
+                    };
+                  };
+                  system.stateVersion = 5;
+                }
+              ];
+            }).system;
+        };
+      };
     };
 }
