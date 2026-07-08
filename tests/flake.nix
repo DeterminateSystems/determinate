c6f51aa7feef386513b20b2a5309e1d555a52361
Cole Helbling <cole.helbling@determinate.systems>
2026-07-08 11:53:27 -0700

    fixup: use appropriate nixpkgs directly

--- tests/flake.nix
+++ tests/flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     determinate.url = "path:../";
-    nixpkgs.follows = "determinate/nix/nixpkgs";
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/=0.2511";
     nix-darwin = {
       url = "https://flakehub.com/f/nix-darwin/nix-darwin/=0.2511";
       inputs.nixpkgs.follows = "nixpkgs";
